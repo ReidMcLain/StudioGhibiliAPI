e089dbedcddd10526ff64192fa239ea2a2324da3
ReidMcLain <reidmm@gmail.com>
2022-03-21 10:32:58 -0500

    added totoro to every page

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
         const response = await fetch('https://ghibliapi.herokuapp.com/people')
         const data = await response.json();
         setPeople(data);
+        setFilmsLoaded(false);
         setPeopleLoaded(true);
     };
 
@@ -21,26 +22,56 @@ const App = () => {
         const response = await fetch('https://ghibliapi.herokuapp.com/films')
         const data = await response.json();
         setFilms(data);
+        setPeopleLoaded(false);
         setFilmsLoaded(true);
     };
 
     if (peopleLoaded) {
         return (
-            people.map(person => <PeopleCard person={person} key={person.id} />)
+            <><div className="d-flex flex-direction-row align-items-center" id="TotoroDiv">
+                <img id="TotoroPic" src={Totoro} />
+            </div>
+                <div className="d-flex flex-direction-row align-items-center  mt-3" id="ButtonDiv">
+                    <button id="peopleBtn" className="btn btn-primary mt-3" onClick={getPeople}>People</button>
+                    <button id="filmsBtn" className="btn btn-primary mt-3" onClick={getFilms}>Films</button>
+                </div>
+                {people.map(person => <PeopleCard person={person} key={person.id} />)}
+            </>
         )
     } else if (filmsLoaded) {
         return (
-            films.map(film => <FilmCard film={film} key={film.id} />)
+            <><div className="d-flex flex-direction-row align-items-center" id="TotoroDiv">
+                <img id="TotoroPic" src={Totoro} />
+            </div>
+                <div className="d-flex flex-direction-row align-items-center  mt-3" id="ButtonDiv">
+                    <button id="peopleBtn" className="btn btn-primary mt-3" onClick={getPeople}>People</button>
+                    <button id="filmsBtn" className="btn btn-primary mt-3" onClick={getFilms}>Films</button>
+                </div>
+                {films.map(film => <FilmCard film={film} key={film.id} />)}
+            </>
         )
     } else {
         return (
             <><div className="d-flex flex-direction-row align-items-center" id="TotoroDiv">
                 <img id="TotoroPic" src={Totoro} />
             </div>
-                <div className="d-flex flex-direction-row align-items-center" id="TotoroDiv">
-                    <button onClick={getPeople}>People</button>
-                    <button onClick={getFilms}>Films</button>
+                <div className="d-flex flex-direction-row align-items-center  mt-3" id="ButtonDiv">
+                    <button id="peopleBtn" className="btn btn-primary mt-3" onClick={getPeople}>People</button>
+                    <button id="filmsBtn" className="btn btn-primary mt-3" onClick={getFilms}>Films</button>
                 </div>
+                <main className="container">
+                    <section className="row justify-content-center mt-5">
+                        <div className="col-md-6">
+                            <div className="card shadow my-2">
+                                <div className="card-body">
+                                    <h4 className="d-flex flex-direction-row align-items-center" id="frontPageCardTitle">Studio Ghibli API Lab</h4>
+                                    <p className="card-text">This labs shows off my understanding of fetching data through API's and conditional rending items on a page through React :)</p>
+                                    <p className="text-muted">Check out more cool projects at https://github.com/ReidMcLain</p>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </main>
             </>
         )
     }
